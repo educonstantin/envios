8a51e2bcf2541b4f5d9764cf7ab84b03e29502fa
Eduardo Luis Constantin <educonstantin@gmail.com>
2019-01-26 10:00:51 -0300

    Add files via upload

--- consignment-service/handler.go
+++ consignment-service/handler.go
@@ -1,10 +1,8 @@
-// consignment-service/service.go
 package main
 
 import (
-
 	"log"
-	
+
 	pb "github.com/educonstantin/envios/consignment-service/proto/consignment"
 	vesselProto "github.com/educonstantin/envios/vessel-service/proto/vessel"
 	"golang.org/x/net/context"
@@ -12,11 +10,11 @@ import (
 )
 
 // Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can chack the interface
+// we defined in our protobuf definition. You can check the interface
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
 type service struct {
-	session *mgo.Session
+	session      *mgo.Session
 	vesselClient vesselProto.VesselServiceClient
 }
 
@@ -24,12 +22,14 @@ func (s *service) GetRepo() Repository {
 	return &ConsignmentRepository{s.session.Clone()}
 }
 
-// CreateConsignment - we create just one methos on our service,
-// which is a create methos, which takes a context and a request as an
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+func (s *service) Create(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 	repo := s.GetRepo()
 	defer repo.Close()
+
+	log.Println("Creating: ", req)
 
 	// Here we call a client instance of our vessel service with our consignment weight,
 	// and the amount of containers as the capacity value
@@ -37,10 +37,10 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 		MaxWeight: req.Weight,
 		Capacity:  int32(len(req.Containers)),
 	})
-	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
 	if err != nil {
 		return err
 	}
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
 
 	// We set the VesselId as the vessel we got back from our
 	// vessel service
@@ -52,16 +52,17 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 		return err
 	}
 
-	// Return matching the 'Response' message we created in our
+	// Return matching the `Response` message we created in our
 	// protobuf definition.
 	res.Created = true
 	res.Consignment = req
 	return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+func (s *service) Get(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
 	repo := s.GetRepo()
 	defer repo.Close()
+
 	consignments, err := repo.GetAll()
 	if err != nil {
 		return err
@@ -69,4 +70,3 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 	res.Consignments = consignments
 	return nil
 }
-
