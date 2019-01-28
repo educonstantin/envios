6c272771bbcfa5d7e4649b858abdc85df15ad1c1
Eduardo Luis Constantin <educonstantin@gmail.com>
2019-01-28 12:38:25 -0300

    Update main.go

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -51,7 +51,7 @@ func main() {
 	}
 
 	// Create a new service. Optionally include some options here.
-	srv = k8s.NewService(
+	srv = micro.NewService(
 
 		// This name must match the package name given in your protobuf definition
 		micro.Name("consignment"),
